80f4cc102858d40641259d6038efc06944e5d92a
Michael-Bodzenski <94140238+Michael-Bodzenski@users.noreply.github.com>
2021-11-14 20:10:32 -0500

    v2

--- project2.cpp
+++ project2.cpp
@@ -4,6 +4,49 @@
 
 using namespace std;
 
+double maxAlignment(vector<double> seq1, int n, vector<double> seq2, int m, vector<double> target, vector<vector<double>> &array, int x, int y)
+{
+    cout << "start func" << endl;
+
+    double z;
+    if(array[x][y] != 0)
+        return array[x][y];
+    else if (x == n + 2 || y == m + 2 || (x == n + 1 && y == m + 1))
+        array[x][y] = 0;
+    else
+    {
+        z = max( (seq1[x] * target[x+y]) + maxAlignment(seq1, n, seq2, m, target, array, x + 1, y), (seq2[y] * target[x+y]) + maxAlignment(seq1, n, seq2, m, target, array, x, y + 1));
+        array[x][y] = z;
+    }
+    
+    cout << array[x][y] << endl;
+    return array[x][y];
+
+    cout << "end func" << endl;
+}
+
+double maxAlignmentWrapper(vector<double> seq1, int n, vector<double> seq2, int m, vector<double> target)
+{
+    cout << "start wrapper" << endl;
+
+    vector<vector<double>> array(n + 2, vector<double>(m + 2));
+
+    for(int i = 0; i < n + 2; i++)
+    {
+        for(int j = 0; j < m + 2; j++)
+        {
+            array[i][j] = 0;
+        }
+    }
+    cout << "before call" << endl;
+
+    double max = maxAlignment(seq1, n, seq2, m, target, array, 0, 0);
+
+    cout << "end wrapper" << endl;
+
+    return max;
+}
+
 int main()
 {
     //read file and fill: n, m, seq1, seq2, and target
@@ -15,32 +58,8 @@ int main()
     vector<double> seq2{1, 3, 6};
     vector<double> target{1, 2, 1, 3, 7, 6};
 
+    cout << "in main" << endl;
 
     double max = maxAlignmentWrapper(seq1, n, seq2, m, target);
     cout << max << endl;
-}
-
-int maxAlignmentWrapper(vector<double> seq1, int n, vector<double> seq2, int m, vector<double> target)
-{
-    vector<vector<double>> array;
-
-    for(int i = 0; i < n + 2; i++)
-        for(int j = 0; j < m + 2; j++)
-            array[i][j] = ;
-
-    return maxAlignment(seq1, n, seq2, m, target, array, 0, 0);
-}
-
-int maxAlignment(vector<double> seq1, int n, vector<double> seq2, int m, vector<double> target, vector<vector<double>> &array, int x, int y)
-{
-    double z;
-    if(array[x][y] != NULL)
-        return array[x][y];
-    else if (x == n + 2 || y == m + 2 || (x == n + 1 && y == m + 1))
-        array[x][y] = 0;
-    else
-        z = max( (seq1[x] * target[x+y]) + maxAlignment(seq1, n, seq2, m, target, array, x + 1, y), (seq2[y] * target[x+y]) + maxAlignment(seq1, n, seq2, m, target, array, x, y + 1));
-        array[x][y] = z;
-    
-    return array[x][y];
 }
